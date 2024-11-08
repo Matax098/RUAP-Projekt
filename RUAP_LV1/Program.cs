f4629270317a2efed0dd961cbaacd4c11842d66a
Matax098 <matijafidija@gmail.com>
2024-11-08 16:24:16 +0100

    Nakon promjene

--- RUAP_LV1/Program.cs
+++ RUAP_LV1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Pozdrav od Matije Vidovića");
         }
     }
 }
